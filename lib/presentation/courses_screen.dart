2b4ea351408314fad9f76f77ee5ca5e20ca4ea07
20120558 <luungocquang2002@gmail.com>
2023-10-26 14:19:02 +0700

    Added filter list for course

--- lib/presentation/courses_screen.dart
+++ lib/presentation/courses_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:let_tutor/presentation/styles/custom_text_style.dart';
 
 class CoursesScreen extends StatefulWidget {
   const CoursesScreen({super.key});
@@ -8,6 +9,44 @@ class CoursesScreen extends StatefulWidget {
 }
 
 class _CoursesScreenState extends State<CoursesScreen> {
+  bool visibilityFilter = false;
+  List<String> levels = [
+    'All level',
+    'Beginner',
+    'Upper-Beginner',
+    'Pre-Intermediate',
+    'Intermediate',
+    'Upper-Intermediate',
+    'Pre-Advanced',
+    'Advanced',
+    'Very Advanced'
+  ];
+  List<String> categories = [
+    'All categories',
+    'For Studying Abroad',
+    'English For Traveling',
+    'Conversational English',
+    'English For Beginners',
+    'Business English',
+    'English For Kids',
+    'STARTERS',
+    'PET',
+    'KET',
+    'MOVERS',
+    'FLYERS',
+    'TOEFL',
+    'TOEIC',
+    'IELTS'
+  ];
+  List<String> sortLevels = [
+    'Sort by level',
+    'Level decreasing',
+    'Level increasing'
+  ];
+  late String _selectedLevel = levels[0];
+  late String _selectedCategory = categories[0];
+  late String _selectedSortLevel = sortLevels[0];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -70,20 +109,26 @@ class _CoursesScreenState extends State<CoursesScreen> {
       padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       child: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.only(right: 24),
-              hintStyle: TextStyle(color: Colors.grey[400]),
-              hintText: 'search courses',
-              prefixIcon: const Icon(Icons.search),
-              border: const OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.grey, width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-            ),
-          ),
+          _filterBar(),
           const SizedBox(height: 8),
-          Expanded(
-            child: Text('Courses'),
+          Visibility(
+            visible: visibilityFilter,
+            child: Column(
+              children: [
+                levelFilter(),
+                const SizedBox(
+                  height: 8,
+                ),
+                categoriesFilter(),
+                const SizedBox(
+                  height: 8,
+                ),
+                sortLevel(),
+                const SizedBox(
+                  height: 8,
+                )
+              ],
+            ),
           )
         ],
       ),
@@ -111,6 +156,104 @@ class _CoursesScreenState extends State<CoursesScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  _filterBar() {
+    return Row(
+      children: [
+        const Expanded(
+          flex: 1,
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.only(right: 24),
+              hintStyle: TextStyle(color: Color(0xFFB0B0B0)),
+              hintText: 'Enter a course name',
+              prefixIcon: Icon(
+                Icons.search,
+                color: Color(0xFFB0B0B0),
+              ),
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFB0B0B0), width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(8))),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 16,
+        ),
+        IconButton(
+          icon: const Icon(Icons.filter_list_outlined),
+          onPressed: () {
+            setState(() {
+              visibilityFilter = !visibilityFilter;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget levelFilter() {
+    return DropdownButton<String>(
+      isExpanded: true,
+      value: _selectedLevel,
+      items: List.generate(
+        levels.length,
+        (index) => DropdownMenuItem(
+          value: levels[index],
+          child: Text(
+            levels[index],
+          ),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _selectedLevel = value!;
+        });
+      },
+    );
+  }
+
+  categoriesFilter() {
+    return DropdownButton<String>(
+      isExpanded: true,
+      value: _selectedCategory,
+      items: List.generate(
+        categories.length,
+        (index) => DropdownMenuItem(
+          value: categories[index],
+          child: Text(
+            categories[index],
+          ),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _selectedCategory = value!;
+        });
+      },
+    );
+  }
+
+  sortLevel() {
+    return DropdownButton<String>(
+      isExpanded: true,
+      value: _selectedSortLevel,
+      items: List.generate(
+        sortLevels.length,
+        (index) => DropdownMenuItem(
+          value: sortLevels[index],
+          child: Text(
+            sortLevels[index],
+          ),
+        ),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _selectedSortLevel = value!;
+        });
+      },
     );
   }
 }
